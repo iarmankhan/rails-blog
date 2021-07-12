8eadd277200ae2fe9f4176a4be4b33b4e4d68393
iarmankhan <work.armankhan@gmail.com>
2021-07-12 23:14:08 +0530

    :sparkles: article controller refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,18 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
-  def new
-
-  end
+  def new; end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = 'Article was created successfully'
       redirect_to @article
@@ -24,12 +23,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-    @article.update(params.require(:article).permit(:title, :description))
+    @article.update(article_params)
     if @article.save
       flash[:notice] = 'Article updated successfully'
       redirect_to @article
@@ -39,10 +36,19 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
