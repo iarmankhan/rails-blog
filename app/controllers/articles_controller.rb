64b0bf363b8a45a0b42622e844011fea25608790
iarmankhan <work.armankhan@gmail.com>
2021-07-12 22:20:42 +0530

    :sparkles: new article with validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,21 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = 'Article was created successfully'
+      redirect_to @article
+    else
+      render 'new'
+    end
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
-  resources :articles, only: %i[index show]
+  resources :articles, only: %i[index show create new]
 end
