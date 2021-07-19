a0fcb1616aa8420ee7d6f82ea218eb7bcd2b0839
iarmankhan <work.armankhan@gmail.com>
2021-07-19 21:59:20 +0530

    :sparkles: user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      flash[:notice] = "Welcome to the alpha blog #{@user.username}! You've successfully signed up"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
+
 end
