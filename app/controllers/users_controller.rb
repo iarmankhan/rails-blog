26c23de0f910b56295dadc97f36ae4e087c6688c
iarmankhan <work.armankhan@gmail.com>
2021-08-02 22:13:33 +0530

    :sparkles: getting the current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
   def new
     @user = User.new
   end
@@ -7,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the alpha blog #{@user.username}! You've successfully signed up"
       redirect_to articles_path
     else
@@ -14,13 +17,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       flash[:notice] = 'Your account information updated successfully!'
       redirect_to @user
@@ -30,7 +29,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -42,6 +40,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :password, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
