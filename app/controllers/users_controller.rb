29cf70a98b31189e55da856a2f20c27702c8bdc5
iarmankhan <work.armankhan@gmail.com>
2021-08-03 23:34:53 +0530

    :sparkles: delete user and articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update]
+  before_action :set_user, only: %i[show edit update destroy]
   before_action :require_user, only: %i[edit update]
-  before_action :require_same_user, only: %i[edit update]
+  before_action :require_same_user, only: %i[edit update destroy]
 
   def new
     @user = User.new
@@ -36,6 +36,13 @@ class UsersController < ApplicationController
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Account and all associate articles are deleted'
+    redirect_to root_path
   end
 
   private
